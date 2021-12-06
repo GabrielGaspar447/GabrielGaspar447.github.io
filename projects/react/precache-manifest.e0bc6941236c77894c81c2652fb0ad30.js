4c18debb5ee9ef9a00bad460c3b24304bf7696a0
Gabriel Gaspar <83838669+GabrielGaspar447@users.noreply.github.com>
2021-12-06 03:02:49 -0300

    teste2

---
+++ projects/react/precache-manifest.e0bc6941236c77894c81c2652fb0ad30.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41efb1c0024af1e5117d3efb7deaa85",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "c1f2743ccaf032726f2a",
+    "url": "/recipe-app/static/css/main.8406d388.chunk.css"
+  },
+  {
+    "revision": "50531d58da620a83f328",
+    "url": "/recipe-app/static/js/2.33ed4496.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipe-app/static/js/2.33ed4496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f2743ccaf032726f2a",
+    "url": "/recipe-app/static/js/main.c7be4e36.chunk.js"
+  },
+  {
+    "revision": "fc6e5076bcefd61482c9",
+    "url": "/recipe-app/static/js/runtime-main.b4c1aa53.js"
+  },
+  {
+    "revision": "fed972920c028a3e1fc6065618e170c6",
+    "url": "/recipe-app/static/media/blackHeartIcon.fed97292.svg"
+  },
+  {
+    "revision": "e20c3bb888d5148078d29eff51b605bc",
+    "url": "/recipe-app/static/media/drinkIcon.e20c3bb8.svg"
+  },
+  {
+    "revision": "8486ec46bdd2f85a74769895b7d1ba2c",
+    "url": "/recipe-app/static/media/exploreIcon.8486ec46.svg"
+  },
+  {
+    "revision": "0a218dad3829d89bcf72675425a6b5fd",
+    "url": "/recipe-app/static/media/mealIcon.0a218dad.svg"
+  },
+  {
+    "revision": "8712a673584ce3ba8d57bfcba4860342",
+    "url": "/recipe-app/static/media/profileIcon.8712a673.svg"
+  },
+  {
+    "revision": "4552d4406cecc569cca33b4f1d54733f",
+    "url": "/recipe-app/static/media/searchIcon.4552d440.svg"
+  },
+  {
+    "revision": "bede90052e7ec3f6268f45752c94144b",
+    "url": "/recipe-app/static/media/shareIcon.bede9005.svg"
+  },
+  {
+    "revision": "7e5b379e5ef0875091b4d7b0291f3822",
+    "url": "/recipe-app/static/media/whiteHeartIcon.7e5b379e.svg"
+  }
+]);
